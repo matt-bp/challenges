2e794ab59664339aca50ca26d69cc5de4dd03df4
matt-bp <matt.bishop@readinghorizons.com>
2022-12-01 09:51:36 -0700

    Adding post challenges

---
+++ AdventOfCode/2021-post/01/Models/CLIParameters.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+
+namespace _01.Models
+{
+    internal class CLIParameters
+    {
+        [Option('r', "read", Required = true, HelpText = "Input files to be processed.")]
+        public IEnumerable<string> InputFiles { get; set; }
+    }
+}
---
+++ AdventOfCode/2021-post/01/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CommandLine;
+using _01.Models;
+
+Parser.Default.ParseArguments<CLIParameters>(args)
+  .WithParsed(RunOptions)
+  .WithNotParsed(HandleParseError);
+
+static void RunOptions(CLIParameters opts)
+{
+	//handle options
+	var input = File.ReadLines(opts.InputFiles.First()).ToList();
+}
+static void HandleParseError(IEnumerable<Error> errs)
+{
+	//handle errors
+	foreach(var error in errs)
+    {
+		Console.WriteLine(error);
+    }
+}
