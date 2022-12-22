d80a53f92b405c2a3966c23deb2dcb4597367843
matt-bp <matt.bishop@readinghorizons.com>
2022-12-21 17:30:59 -0700

    Starting day 11

---
+++ AdventOfCode/2022/11/Parts/PartOne.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00.Parts;
+
+public static class PartOne
+{
+    public static void Run(string[] lines)
+    {
+        Console.WriteLine("===== PART ONE =====");
+
+        // TODO: Create monkey list
+    }
+}
---
+++ AdventOfCode/2022/11/Program.cs
@@ -0,0 +1,29 @@
+﻿using CommandLine;
+using _00;
+using _00.Parts;
+using Shared.Models;
+using System.Linq;
+
+Parser.Default.ParseArguments<Options>(args)
+    .WithParsed(RunOptions)
+    .WithNotParsed(HandleParseError);
+
+static void RunOptions(Options opts)
+{
+    var exists = File.Exists(opts.InputFile);
+    if (!exists)
+    {
+        Console.WriteLine("The file doesn't exist.");
+        return;
+    }
+
+    var lines = File.ReadAllLines(opts.InputFile);
+
+    PartOne.Run(lines);
+    PartTwo.Run();
+}
+
+static void HandleParseError(IEnumerable<Error> errs)
+{
+    Console.WriteLine("Error!");
+}
