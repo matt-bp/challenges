6e1836fedcabf29a0ff699b8c2ae07f407eaa3bc
matt-bp <matt.bishop@readinghorizons.com>
2023-07-11 13:46:08 -0600

    Started project euler

---
+++ projecteuler/csharp/pe001/Program.cs
@@ -0,0 +1,4 @@
+﻿
+var result = Enumerable.Range(0, 1000).Where(x => x % 3 == 0 || x % 5 == 0).Sum();
+
+Console.Write(result);
---
+++ projecteuler/csharp/pe002/Program.cs
@@ -0,0 +1,22 @@
+﻿var results = new Dictionary<int, int>
+{
+    { 0, 1 },
+    { 1, 2 },
+};
+
+var currentResult = 2;
+
+var sum = 2;
+
+
+for (var i = 2; currentResult < 4000000; i++)
+{
+    currentResult = results[i - 1] + results[i - 2];
+
+    results[i] = currentResult;
+
+    if (int.IsEvenInteger(currentResult))
+        sum += currentResult;
+}
+
+Console.WriteLine(sum);
