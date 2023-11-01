7988ac1e14f174ce9e6e6b34e2fe27e78caa60a3
matt-bp <matt.bishop@readinghorizons.com>
2023-11-01 10:42:36 -0600

    Adding back in the actual code.

---
+++ codewars/csharp/kata-array-diff.library/Kata.cs
@@ -0,0 +1,13 @@
+﻿namespace kata_array_diff.lib;
+
+using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static int[] ArrayDiff(int[] a, int[] b)
+    {
+        var bList = b.ToList();
+        return a.ToList().Where(v => !bList.Any(bv => bv == v)).ToList().ToArray();
+    }
+}
---
+++ codewars/csharp/kata-array-diff.library/Others.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kata_array_diff.lib
+{
+    public class Others
+    {
+        public static int[] SolutionArrayDiff(int[] a, int[] b)
+        {
+            // With a hashset, we won't have to iterate over b for every item in a.
+            // Instead, we can check if an item exists in constant time
+            HashSet<int> bSet = new HashSet<int>(b);
+
+            return a.Where(v => !bSet.Contains(v)).ToArray();
+        }
+
+        public static int[] ArrayContainsArrayDiff(int[] a, int[] b)
+        {
+            return a.Where(n => !b.Contains(n)).ToArray();
+        }
+
+        public static int[] FindAllArrayDiff(int[] a, int[] b)
+        {
+            var sb = new HashSet<int>(b);
+            return Array.FindAll(a, x => !sb.Contains(x));
+        }
+    }
+}
