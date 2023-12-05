6e9eb5a2831de431e64117bc3cd338e299a08c3d
matt-bp <matt.bishop@readinghorizons.com>
2023-12-05 15:23:35 -0700

    Trying to get more visibilty into what's going wrong.

--- AdventOfCode/2023-cpp/day1/day1-app/main.cpp
+++ AdventOfCode/2023-cpp/day1/day1-app/main.cpp
@@ -64,52 +64,53 @@ std::string get_two_digit_p1(const std::string &str) {
 }
 
 std::string get_two_digit_p2(const std::string &str) {
-  std::regex digits_regex("[0-9]|one|two|three|four|five|six|seven|eight|nine");
+  std::regex digits_regex("[1-9]|one|two|three|four|five|six|seven|eight|nine");
   std::smatch digits_match;
 
   std::optional<char> first;
   std::optional<char> second;
 
+  std::cout << str << '\n';
+
   const auto end = std::sregex_iterator();
   for (auto i = std::sregex_iterator(str.begin(), str.end(), digits_regex);
        i != end; ++i) {
     const std::smatch &m = *i;
-    std::cout << m.str() << " at position " << m.position() << '\n';
+    std::cout << "\t" << m.str() << " at position " << m.position() << '\n';
 
     const auto &c = get_digit(m.str());
 
     if (!first.has_value()) {
       first = c;
+      second = c;
     } else {
       second = c;
     }
   }
 
+  std::cout << "  chose: " << first.value() << "," << second.value() << '\n';
+
   return combine_pair(first, second);
 }
 
 char get_digit(const std::string &str) {
-  if (str.size() == 1 && std::isdigit(str.at(0))) {
-    return str[0];
-  }
-
-  if (str == "one") {
+  if (str == "one" || str == "1") {
     return '1';
-  } else if (str == "two") {
+  } else if (str == "two" || str == "2") {
     return '2';
-  } else if (str == "three") {
+  } else if (str == "three" || str == "3") {
     return '3';
-  } else if (str == "four") {
+  } else if (str == "four" || str == "4") {
     return '4';
-  } else if (str == "five") {
+  } else if (str == "five" || str == "5") {
     return '5';
-  } else if (str == "six") {
+  } else if (str == "six" || str == "6") {
     return '6';
-  } else if (str == "seven") {
+  } else if (str == "seven" || str == "7") {
     return '7';
-  } else if (str == "eight") {
+  } else if (str == "eight" || str == "8") {
     return '8';
-  } else if (str == "nine") {
+  } else if (str == "nine" || str == "9") {
     return '9';
   } else {
     throw std::runtime_error("Achk!");
@@ -121,12 +122,7 @@ std::string combine_pair(std::optional<char> first, std::optional<char> second)
   std::stringstream ss;
 
   ss << first.value();
-
-  if (!second.has_value()) {
-    ss << first.value();
-  } else {
-    ss << second.value();
-  }
+  ss << second.value();
 
   return ss.str();
 }
